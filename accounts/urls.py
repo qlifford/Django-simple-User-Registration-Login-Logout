a616f562f0d30d089ec3cbfa8a54e8c08b2eebe3
qlifford <otienocee@gmail.comgit config --global user.nameqlofford>
2023-12-20 21:01:27 +0300

    commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('product/', views.product, name = 'products'),
+    path('register/', views.register, name = 'register'),
+    path('login/', views.login_user, name = 'login_user'),
+    path('register/', views.register, name = 'register'),
+    path('logout_user/', views.logout_user, name = 'logout_user'),
+    path('create_customer/', views.create_customer, name = 'create_customer'),
     path('customer/<str:id>/', views.customer, name = 'customer'),
     path('create_order/<int:id>/', views.create_order, name = 'create_order'),
     path('update_order/<str:id>/', views.update_order, name = 'update_order'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import *
-from .forms import OrderForm
+from .forms import OrderForm, CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 def home(request):
@@ -15,6 +19,7 @@ def home(request):
 def product(request):
     return render(request, 'accounts/product.html')
 
+@login_required(login_url='login_user')
 def customer(request, id):
     customer = Customer.objects.get(id=id)
     orders = customer.order_set.all()
@@ -22,6 +27,7 @@ def customer(request, id):
     context = {'customer': customer, 'orders': orders, 'order_count': order_count}
     return render(request, 'accounts/customer.html', context)
 
+@login_required(login_url='login_user')
 def create_order(request, id):
     customer = Customer.objects.get(id=id)
     form = OrderForm(initial={'customer': customer})
@@ -33,6 +39,7 @@ def create_order(request, id):
     context = {'form':form}
     return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login_user')
 def update_order(request, id):
     order = Order.objects.get(id=id)
     form = OrderForm(instance=order)
@@ -44,10 +51,54 @@ def update_order(request, id):
     context = {'form':form}
     return render(request, 'accounts/order_form.html', context)
 
+@login_required(login_url='login_user')
 def delete_order(request, id):
     order = Order.objects.get(id=id)
     if request.method == 'POST':
         order.delete()
+        messages.info(request, 'You deleted an order!')
         return redirect('home')
     context = {'item':order}
     return render(request, 'accounts/delete.html', context)
+
+def create_customer(request):
+    customers = Customer.objects.all()
+    context = {'customers':customers}
+    return render(request, 'accounts/create_customer.html', context)
+    return render(request, 'accounts/delete.html', context)
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data['username']
+                messages.success(request, 'Registration successful')
+                return redirect('home')
+        context = {'form': form}
+        return render(request, 'registration/register.html', context)
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Wrong username or password')
+                return redirect('login_user')
+        context = {}
+        return render(request, 'registration/login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
